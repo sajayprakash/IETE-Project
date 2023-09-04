6cf83f997fcc605f34861349eaee419d4ee50b91
sajayprakash <sajayprakash7@protonmail.com>
2023-09-04 19:18:04 +0530

    Merge Sanjeev's Contact page code to fix styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,41 +1,66 @@
+import React from 'react'
+import './Contact.css'
+import love from '../images/love.jpg'
+import email from '../images/email.jpg'
+import map from '../images/map.jpg'
 
-import '../components/Contact.css'
-import Footer from './Footer'
-import Navbar from './Navbar'
+const Contact = () => {
+    return (
+        <div>
 
-export default function Contact(){
-    return  <>
-    <Navbar />
-    
-    <h1 style={{fontSize:"30px",fontWeight:"bold"}}>Contact us for more</h1>
-    <p>Feel free to contact us</p>
-    <form>
-           <div id="hello">
-           <input type="text" placeholder="Contact-name"/><br/>
-           
-            <input type="text" placeholder="Phone-number"/><br/>
-            <input type="email" placeholder="Email"/><br/>
-        <input type="text" placeholder="Message"/>  <br/>
-        <br/>
-      <button id="qwerty">Submit</button>    
-      </div>     
-    </form>	
-    <div id="op2">
-    <img src="https://www.freeiconspng.com/uploads/phone-icon-clip-art--royalty--7.png" height="25px" width="25 px"/><p>Phone<br/><span style={{color: "blue"}}>111111</span></p>
-    
-    
-    <img src="https://banner2.cleanpng.com/20190225/pju/kisspng-email-transparency-portable-network-graphics-compu-5c7435d8c2cc18.5946210615511198327979.jpg"height="25px"width="25px"/><p>Email<br/><a href="info@company.com">info@company.com</a></p>
-    
-</div>
-<div id="qw">
-<img src="https://files.propertywala.com/maps/202305/P7245436.jpg" height="350px"width="450px" style={{position:"absolute",bottom:"280px"}} id="meow"/>
-<address>
-    Iete  Chennai Center,<br/>
-    No:137,Conron Smith Road Center,<br/>
-    Gopalapuram,Chennai-86.
-</address>
-</div>
+            <br></br>
+            <br></br>
 
-<Footer/>
-    </>  
- }
+            <div className='fleeex'>
+                <div className='boxx1'>
+                    <h1>Contact us for more</h1>
+                    <div className='space'></div>
+                    <p className='subtxtttt'>Enim tempor eget pharetra facilisis sed maecenas adipiscing. Eu leo molestie vel, ornare non id blandit netus.</p>
+
+                    <form  >
+                        <div id="hello"><br></br><br></br>
+                            <input type="text" placeholder="Contact-name"></input><br></br><br></br>
+
+                            <input type="text" placeholder="Phone-number"></input><br></br><br></br>
+                            <input type="email" placeholder="Email"></input><br></br><br></br>
+                            <input type="text" placeholder="Message"></input> <br></br><br></br>
+                            <br></br>
+                            <button id="qwerty">Submit</button>
+                        </div>
+
+                    </form>
+
+                    <div id="op2" className='space'>
+                        <div className='rowwww'>
+                            <img className='love' src={love} ></img>
+                            <p>Phone<br></br><span className='blue' >111111</span></p>
+                        </div>
+
+
+                        <div className='rowwww'>
+                            <img className='email' src={email} ></img>
+                            <p>Email<br></br><a href="info@company.com">info@company.com</a></p>
+                        </div>
+
+                    </div>
+                </div>
+                <div>
+                    <div id="qw">
+                        <img className='slice spacex' src={map} id="meow"></img>
+                        <address>
+                            Iete Chennai Center,<br></br>
+                            No:137,Conron Smith Road Center,<br></br>
+                            Gopalapuram,Chennai-86.
+                        </address>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Contact
