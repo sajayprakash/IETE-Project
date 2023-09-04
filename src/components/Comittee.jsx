c6d3b3d73015b4f26595844fc3ee070ddd021026
sajayprakash <sajayprakash7@protonmail.com>
2023-09-04 14:04:56 +0530

    Merge code from Sanjeev and Purushothaman

---
+++ src/components/Comittee.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Comittee_Card from './Comittee_Card'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+const Comittee = () => {
+    return (
+       <>
+        <Navbar></Navbar>
+        <div className='section'>
+            <div>
+                <center><h1>COMITTEE MEMBERS</h1></center>
+            </div>
+            <div>
+                <Comittee_Card></Comittee_Card>
+                <Footer></Footer>
+            </div>
+        </div>
+        </>
+    )
+
+}
+
+export default Comittee
---
+++ src/components/Events.jsx
@@ -0,0 +1,118 @@
+// import Events from './Events'
+import HorizontalScroll from "./HorizontalScroll"
+import Navbar from "./Navbar"
+export default function Events(){
+
+    return (
+        <>  <Navbar />
+        
+          <div className='spacee'></div>
+            <div className='width'>
+                <center>
+                    <div className='fc borderr'>
+                        <h1>Webinar on IoT Security(WISE-2022)</h1>
+                        <h3>on 30th May, 2022</h3>
+                        <div className='space'></div>
+                        <h2>SETS Chennai & C-DAC Hyderabad in collaboration with ISEA</h2>
+                        <h2 className='fourchild'>All interested members are requested to register at the following link to participate in the webinar</h2>
+                        <div className='space'></div>
+                        <button className='btntxt'>Weblink</button>
+                        <div className='space'></div>
+                        <h2>Participation certificate will be issued to all the registered participants after attending all the sessions.</h2>
+                    </div>
+                </center>
+                <center>
+                    <div className='fc borderr'>
+                        <h1>IETE CHENCON 2022 - Call for Papers</h1>
+                        <h3>Hybrid Mode- MAY 20 & 21, 2022</h3>
+                        <h2>"International Conference on Power of Artificial Intelligence and Machine Learning for Human Empowerment"</h2>
+                        <div className='space'></div>
+                        <div className='fr'>
+                            <button className='btntxt'>Pdf brochure</button>
+                            <button className='btntxt'>Inagural Session</button>
+                            <button className='btntxt'>Chencon 2022</button>
+                        </div>
+                        <div className='spacee'></div>
+                        <div className='spacee'></div>
+                        <img src=''></img>
+                        <div className='fr'>
+                            <left><h2 className='subtext'>Scope of the Conference</h2></left>
+                            <div></div>
+                        </div>
+                        <div className='space'></div>
+                        <div className='fr'>
+                            <left><h3 className='subtexttxt'>Recently, the use of AI/ML technology has been exponentially increasing in our daily lives. AI/ML applications represent an important evolution in computer science and data processing that is quickly transforming a vast array of domains. IETE CHENCON 2022 provides vibrant opportunities for the delegates to exchange new ideas, applications, practical challenges encountered and to leverage AI and ML technologies. The technical program of the conference will consist of invited key note talks and paper presentations. IETE CHENCON 2022 accepts technical paper presentation from research scholars, faculty, students and industry professionals in emerging areas of AI and ML applications but not limited to:</h3></left>
+                            <div></div>
+                        </div>
+                        <div className='space'></div>
+                        <img src={event}></img>
+                        <div className='spacee'></div>
+                        <div className='spacee'></div>
+                        <div className='fr'>
+                            <left><h2 className='subtext'>Submission Guidelines</h2></left>
+                            <div></div>
+                        </div>
+                        <div className='space'></div>
+                        <div className='fr'>
+                            <left><h3 className='subtexttxt'>Prospective authors are invited to submit full papers describing original, previously unpublished, complete work in four pages in A4 IEEE double column conference template, including figure, results and references. Submitted papers will be reviewed by two to three technical committee or reviewers. Accepted and registered papers of IETE CHENCON 2022 will be published in the conference proceedings. Extended versions of the accepted and presented papers will be recommended for publication in IETE Journals, SCOPUS, SCI and other indexed journals. Authors must submit the papers through their Easy chair account using the following link: https://easychair.org/conferences/?conf=ietechencon2022 or submit the papers to the Email ID: chencon2022@gmail.com</h3></left>
+                            <div></div>
+                        </div>
+                        <div className='spacee'></div>
+                        <div className='spacee'></div>
+                        <div className='innerbox borderr fclasst' id='noborder'>
+                            <div className='frlast' id='color'>
+                                <h2>Registration Fee & Details</h2>
+                                <h2>Registration Fee & Details</h2>
+                            </div>
+                            <div className='frlast'>
+                                <h3 className='qw'>At least one author for each paper should register for the conference.If an author has more than one accepted paper,registration fee has to be paid for each Paper ID.IETE members including ISF : Rs. 1500 + GSTNON IETE participants : Rs. 2000 + GSTInternational Participants : $50</h3>
+                                <h3 className='qe'>Last date for paper Submission : 06.05.2022Notification of Acceptance : 10.05.2022Registration on or before : 15.05.2022Conference Date : 20.05.2022 & 21.05.2022</h3>
+                            </div>
+                        </div>
+                        <div className='space'></div>
+                        <div className='spacee'></div>
+                        <div className='lastbox'>
+                            <div className='leftt'>
+                                <h2>Patron</h2>
+                                <h3>Patron Prof Wg Cdr P. Prabhakar</h3>
+                                <h2>Organizing Chair</h2>
+                                <h3>Dr. N. Venkateswaran</h3>
+                                <h2>Conference Co-Chairs</h2>
+                                <h3>Mrs. T.J. Jeyaprabha,Mr. Arun</h3>
+                                <div className='even'></div>
+                                <div className='flerow'><h2>Mobile:</h2><h3>72000 96048</h3></div>
+                                <div className='flerow'><h2>Phone:</h2><h3>044-28350773</h3></div>
+                                <div className='flerow'><h2>Email:</h2><h3>chencon2021@gmail.com</h3></div>
+                                <div className='flerow'><h2>Website:</h2><h3>http://ietechennai.in    </h3></div>
+                            </div>
+                            <div className='rightt'>
+                                <h2>Advisory Comittee</h2>
+                                <div className='spacee'></div>
+                                <h3>Dr. J.W. Bakal</h3>
+                                <h3>Dr. K.T.V Reddy </h3>
+                                <h3>Dr. Surendra Pal</h3>
+                                <h3>Prof. K.K. Aggarwal </h3>
+                                <h3>Dr. M.H. Kori </h3>
+                                <h3> Dr. Shiv Kumar</h3>
+                                <h3>Dr. K. Jaya Shankar </h3>
+                                <h3>Col. (Retd) K.S. ChakravarthiShri. </h3>
+                                <h3>S. Ramaswamy </h3>
+                                <h3>Dr. Swarna Ravindra Babu </h3>
+                                <h3>Dr. Tata Sudhakar</h3>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </center>
+                <div className='pb'>
+                    <div className='fr'>
+                        <left><h2 className='subtext'>Recent Events</h2></left>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+            <HorizontalScroll></HorizontalScroll>
+        </>
+    )
+ }
